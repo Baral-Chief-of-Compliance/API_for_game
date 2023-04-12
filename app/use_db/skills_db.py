01a74d23ecf6cc3bb2f24f593056484c110576e1
Baral-Chief-of-Compliance <Dartmol21@mail.ru>
2023-04-12 22:43:42 +0300

    add document for add skills

--- app/use_db/skills_db.py
+++ app/use_db/skills_db.py
@@ -3,5 +3,5 @@ from app.use_db.tools import quarry
 
 def create(*args):
     quarry.call("insert into skills(name_s, id_gp, description_gp, min_damage, "
-                "max_damage, chance_crit_interest, max_crit_damage, min_crit_damage) values "
-                "(%s, %s, %s, %s, %s, %s, %s, %s)", *args, commit=True, fetchall=False)
+                "max_damage, chance_crit_interest, max_crit_damage, min_crit_damage, miss_chance) values "
+                "(%s, %s, %s, %s, %s, %s, %s, %s, %s)", *args, commit=True, fetchall=False)
--- skills/skills.py
+++ skills/skills.py
@@ -10,3 +10,16 @@ def all_skills():
     if request.method == "POST":
         name_s = request.json["name_s"]
         id_gp = request.json["id_gp"]
+        description_gp = request.json["description_gp"]
+        min_damage = request.json["min_damage"]
+        max_damage = request.json["max_damage"]
+        chance_crit_interest = request.json["chance_crit_interest"]
+        max_crit_damage = request.json["max_crit_damage"]
+        min_crit_damage = request.json["min_crit_damage"]
+        miss_chance = request.json["miss_chance"]
+
+        skills_db.create([name_s, id_gp, description_gp, min_damage,
+                          max_damage, chance_crit_interest, max_crit_damage,
+                          min_crit_damage, miss_chance])
+
+        return jsonify(f"skills {name_s} for game_person {id_gp} is add")
